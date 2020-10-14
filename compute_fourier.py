7153094b46906602443171f19b852b90ddfa8843
viksit-siddhant <43901482+viksit-siddhant@users.noreply.github.com>
2020-10-15 02:10:31 +0530

    Add files via upload

---
+++ compute_fourier.py
@@ -0,0 +1,28 @@
+import cmath
+import math
+import pickle
+
+#f(x) can be any function defined in [0,1] -> C, which the fourier series will try to approximate
+def f(x,n):
+    y = math.cos(2*math.pi*x) + 2j*math.sin(2*math.pi*x)
+    #Don't change this
+    y *= cmath.exp(-2*math.pi*1j*n*x)
+    return y
+
+list_of_cn = []
+
+def num_integrate(f,lower_lim=0,upper_lim=1,delta_x=0.01,n=None):
+    y = lower_lim
+    result = 0
+    while y <= upper_lim:
+        result += f(y,n)*delta_x
+        y += delta_x
+    return result
+
+for i in range(-50,51):
+    list_of_cn.append(num_integrate(f,n=i,delta_x=0.0005))
+
+with open('cn_square.pkl','wb') as f:
+    pickle.dump(list_of_cn,f)
+
+
---
+++ watch_fourier.py
@@ -0,0 +1,75 @@
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation,style
+import math
+import cmath
+
+list_of_cn = []
+
+with open('cn_square.pkl','rb') as f:
+    list_of_cn = pickle.load(f)
+
+style.use('ggplot')
+fig = plt.figure()
+ax = plt.axes(xlim=(-2,2), ylim=(-2,2))
+
+vectors = []
+
+for i in range(101):
+    line, = ax.plot([], [])
+    vectors.append(line)
+
+path, = ax.plot([],[])
+
+vectors.append(path)
+class Square:
+    def __init__(self):
+        self.mem_x = []
+        self.mem_y = []
+    
+    def f(self,t):
+        results = []
+        for i,j in enumerate(range(-50,51)):
+            results.append(list_of_cn[i]*cmath.exp(2*math.pi*1j*j*t))
+        return results
+
+    def clear_mem(self):
+        self.mem_x = []
+        self.mem_y = []
+
+square = Square()
+
+def init():
+    path.set_data([], [])
+    square.clear_mem()
+
+    return path,
+
+def animate(i):
+    results = square.f(i)
+    x = 0
+    y = 0
+    for i in range(101):
+        prev_x = x
+        prev_y = y
+        x += results[i].real
+        y += results[i].imag
+        vectors[i].set_data([prev_x,x],[prev_y,y])
+    square.mem_x.append(x)
+    square.mem_y.append(y)
+    path.set_data(square.mem_x,square.mem_y)
+    return tuple(vectors)
+
+def gen_x():
+    #Change this to change the speed at which the line moves
+    delta_x = 0.00075
+    num = 0
+    while num <= 1:
+        yield num
+        num += delta_x
+
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=gen_x, interval=5, blit=True)
+
+plt.show()
